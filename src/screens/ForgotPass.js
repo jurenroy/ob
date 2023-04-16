e73278f6eba3f06ecffc7b08fb028d8b453c614f
jurenroy <jurenroyabragn91019@gmail.com>
2023-04-16 22:08:35 +0800

    ednilan merge

--- src/screens/ForgotPass.js
+++ src/screens/ForgotPass.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Headed from "../components/Headed";
-<<<<<<< HEAD
 import styles from "../components/styles.css";
-=======
-import "../components/styles.css";
->>>>>>> origin/Ednilan
 import ob from  "../assets/ob.png";
 
 const ForgotPass = () => {
@@ -12,11 +8,7 @@ const ForgotPass = () => {
         <div style={{ backgroundColor: "lightgray", height:'100vh'}}>
             <Headed/>
             <div className='container'>
-<<<<<<< HEAD
                 <img src={ob} style={{height: '300px', width: '300px', marginTop:'50px', marginLeft: '53rem'}} />
-=======
-                <img src={ob} style={{height: '300px', width: '300px', marginTop:'50px', marginLeft: '40%'}} />
->>>>>>> origin/Ednilan
                 <h1 style={{color: 'white', fontSize: '70px',fontStyle: 'Poppins', marginTop: '22rem', justifyContent: 'center', alignItems: 'center', marginLeft: "-26rem"}}>Forgot Password</h1>
             </div>
             
@@ -26,11 +18,7 @@ const ForgotPass = () => {
             type="type"
             required = 'required'/>
             <span className="emfp">Email Address</span>
-<<<<<<< HEAD
             <button style={{height: '43px', width: '20rem', marginLeft: '53rem', marginTop: '-250PX', position: 'absolute', fontWeight: 'bold'}}>SEND TO EMAIL ADDRESS</button>
-=======
-            <button style={{height: '43px', width: '20rem', marginLeft: '40%', marginTop: '-150PX', position: 'absolute', fontWeight: 'bold'}}>SEND TO EMAIL ADDRESS</button>
->>>>>>> origin/Ednilan
         </div>
     )
 }
--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -2,31 +2,17 @@ import React from "react";
 import Headed from "../components/Headed";
 import styles from "../components/styles.css";
 import ob from  "../assets/ob.png";
-<<<<<<< HEAD
-=======
-import { Link, useNavigate} from "react-router-dom";
->>>>>>> origin/Ednilan
 
 const Dash = () => {
-    const navigate = useNavigate();
 
     return( 
         <div style={{ backgroundColor: "lightgray", height:'100vh'}}>
-<<<<<<< HEAD
             <Headed style={{ position: 'fixed', width: '100%' }} />
             <div className='container'>
           <div className='second'>
             <img src={ob} style={{ height: "200px", width: "200px", marginTop:'10px', justifyContent: 'center', alignItems: 'center'}} />
             <h1 style={{color: 'white', fontSize: '25px', marginTop: '-10px', marginLeft: "-350px", fontWeight: "0px", fontStyle: "Poppins"}}>LOGIN</h1>
             <h3 style={{color: '#828282', fontSize: '15px', marginTop: '-20px', marginLeft: "-170px", fontWeight: "0px", fontStyle: "Poppins"}}>Sign in to continue to our application</h3>
-=======
-            <Headed/>
-            <div className='container'>
-          <div className='second'>
-            <img src={ob} style={{ height: "250px", width: "250px", marginTop:'15px', justifyContent: 'center', alignItems: 'center', marginLeft: '180px'}} />
-            <h1 style={{color: 'white', fontSize: '35px', marginTop: '-10px', marginLeft: "35px", fontWeight: "0px", fontStyle: "Poppins"}}>LOGIN</h1>
-            <h3 style={{color: '#828282', fontSize: '16px', marginTop: '-25px', marginLeft: "35px", fontWeight: "0px", fontStyle: "Poppins"}}>Sign in to continue to our application</h3>
->>>>>>> origin/Ednilan
            
             <input 
             className="email"
@@ -41,7 +27,6 @@ const Dash = () => {
             type="password"
             required = 'required'/>
             <span className="password">Password</span>
-<<<<<<< HEAD
             <a href style= {{textDecorationLine: 'underline', cursor: 'pointer', marginTop: '20%', marginLeft: '330px', textDecorationColor: 'white'}}>
                 <h3 style={{color: 'white', fontWeight: 'normal', fontSize:16}}>Forgot Password? </h3>
             </a>
@@ -49,15 +34,6 @@ const Dash = () => {
             <button style={{height: '43px', width: '91%', marginLeft: '2%'}}>Sign In</button>
             <a href style= {{textDecorationLine: 'underline', cursor: 'pointer', marginTop: '-10px',textDecorationColor: 'white'}}>
                 <h4 style={{color: 'white', fontWeight: 'normal'}}>Doesn’t have an Account? Sign Up Here.</h4>
-=======
-            <a href style= {{textDecorationLine: 'underline', cursor: 'pointer', marginTop: '145px', marginLeft: '410px', textDecorationColor: 'white'}}>
-                <Link to = "/forgotpassword" style={{color: 'white', fontWeight: 'normal'}}>Forgot Password? </Link>
-            </a>
-
-            <button style={{height: '43px', width: '33rem', marginLeft: '35px', marginTop: '5px'}} onClick={() => navigate('/homepage')}>Sign In</button>
-            <a href style= {{textDecorationLine: 'underline', cursor: 'pointer', marginTop: '3px', marginLeft: '175px', textDecorationColor: 'white'}}>
-                <Link to = "/register" style={{color: 'white', fontWeight: 'normal'}}>Doesn’t have an Account? Sign Up Here.</Link>
->>>>>>> origin/Ednilan
             </a>
           </div>
         </div>
