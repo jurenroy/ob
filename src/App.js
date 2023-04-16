3385deadf395e120e3c7b173940d7bb6754208ab
jurenroy <jurenroyabragn91019@gmail.com>
2023-04-16 21:49:23 +0800

    merge trish

--- src/App.js
+++ src/App.js
@@ -3,28 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dash from './screens/dashboard';
 import LoveUs from './screens/Loveus';
 import Dobook from './screens/Dobook.';
-<<<<<<< HEAD
 import ForgotPass from './screens/ForgotPass';
 import MandatoryProf from './screens/MandatoryProf';
-=======
-import Message from './screens/message';
-import Profile from './screens/Profile';
->>>>>>> origin/Trisha
+import Headed from './components/Headed';
 
 function App() {
   return (
     <Router>
       <Routes>
+      <Route exact path="/headz" element={<Headed />} />
         <Route exact path="/" element={<Dash />} />
         <Route path="/loveus" element={<LoveUs />} />
         <Route path="/dobook" element={<Dobook />} />
-<<<<<<< HEAD
         <Route path="/ForgotPass" element={<ForgotPass />} />
         <Route path="/MandatoryProf" element={<MandatoryProf />} />
-=======
-        <Route path="/message" element={<Message/>}/>
-        <Route path="/profile" element={<Profile/>}/>
->>>>>>> origin/Trisha
       </Routes>
     </Router>
   );
